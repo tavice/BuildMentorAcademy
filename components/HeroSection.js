e1d167052a1ba7f78b51a476b717d6654969e590
tavice <thomasavice.ta@gmail.com>
2024-03-30 07:47:10 -0700

    misc svg global css work

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -7,31 +7,30 @@ function HeroSection() {
   const path3 = `M0,0V5.63C149.93,59,314.09,71.32,475.83,42.57c43-7.64,84.23-20.12,127.61-26.46,59-8.63,112.48,12.24,165.56,35.4C827.93,77.22,886,95.24,951.2,90c86.53-7,172.46-45.71,248.8-84.81V0Z`;
   return (
     <>
-        <section className="bg-gradient-to-r from-blue-400 to-orange-400 text-white py-20 rounded-lg">
-      <svg
-        data-name="Layer 1"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 1200 120" 
-        preserveAspectRatio="none"
-      >
-        <path d={path1} opacity=".15" className="shape-fill" />
-        <path d={path2} opacity=".5" className="shape-fill" />
-        <path d={path3} className="shape-fill" />
-      </svg>
+      <section className="bg-gradient-to-r from-blue-400 to-orange-400 text-white pb-20 rounded-lg">
+        <svg
+          data-name="Layer 1"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 1200 120"
+          preserveAspectRatio="none"
+        >
+          <path d={path1} opacity=".15" className="shape-fill" />
+          <path d={path2} opacity=".5" className="shape-fill" />
+          <path d={path3} className="shape-fill" />
+        </svg>
         <div className="container mx-auto text-center">
-          <h1 className="text-4xl font-bold mb-4">
+          <h1 className="text-4xl font-bold mb-4 py-10">
             Empower Your Construction Project Management Skills
           </h1>
-          <p className="text-lg mb-8">
+          <h2 className="text-2xl font-bold mb-4 pb-10">
             Join BuildMentor Academy and unlock your potential in construction
             project management.
-          </p>
+          </h2>
           <button className="bg-blue-500 text-white px-6 py-3 rounded-md hover:bg-blue-300">
             Start Learning
           </button>
         </div>
       </section>
-
     </>
   );
 }
